6c16092ee688b4aa5b26538ba3c2df6905d1da2e
levibibo <levibibo@gmail.com>
2017-02-16 20:19:20 -0800

    Set up Teams model and table.

--- src/ArenaRoster/Controllers/TeamsController.cs
+++ src/ArenaRoster/Controllers/TeamsController.cs
@@ -1,60 +1,65 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-//using ArenaRoster.Models;
-//using Microsoft.AspNetCore.Mvc.Rendering;
-//using System.Diagnostics;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ArenaRoster.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
-//namespace ArenaRoster.Controllers
-//{
-//    public class TeamsController : Controller
-//    {
-//        private readonly ArenaRosterDbContext _db;
-//        private readonly UserManager<ApplicationUser> _userManager;
+namespace ArenaRoster.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly ArenaRosterDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-//        public TeamsController(UserManager<ApplicationUser> userManager, ArenaRosterDbContext db)
-//        {
-//            _userManager = userManager;
-//            _db = db;
-//        }
+        public TeamsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ArenaRosterDbContext db)
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
-//        public IActionResult Index()
-//        {
-//            return View(_db.Teams.ToList());
-//        }
+        public IActionResult Index()
+        {
+            return View(_db.Teams.ToList());
+        }
 
-//        public IActionResult Create()
-//        {
-//            return View();
-//        }
-//        [HttpPost]
-//        public IActionResult Create(Team newTeam)
-//        {
-//            _db.Teams.Add(newTeam);
-//            _db.SaveChanges();
-//            return RedirectToAction("Index");
-//        }
+        public IActionResult Create()
+        {
+            Debug.WriteLine("test");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(User);
+            Player userPlayer = _db.Players.FirstOrDefault(p => p.AppUserId == user.Id);
+            Team newTeam = new Team() { Name = name };
+            _db.Teams.Add(newTeam);
+            _db.SaveChanges();
+            PlayerTeam newPlayerTeam = new PlayerTeam() { Player = userPlayer, Team = newTeam };
+            _db.PlayersTeams.Add(newPlayerTeam);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-//        public IActionResult Details(int id)
-//        {
-//            ViewBag.Players = new SelectList(_db.Players, "PlayerId", "Name");
-//            return View(_db.Teams
-//                .Include(p => p.PlayerTeams)
-//                .ThenInclude(bp => bp.Player)
-//                .FirstOrDefault(team => team.TeamId == id));
-//        }
-//        [HttpPost]
-//        public IActionResult Details(int id, Player selectedPlayer)
-//        {
-//            Debug.WriteLine(selectedPlayer.Name);
-//            //PlayerTeam newPlayerTeam = new PlayerTeam(selectedPlayer.PlayerId, id);
-//            //db.PlayerTeam.Add(newPlayerTeam);
-//            //db.SaveChanges();
-//            return RedirectToAction("Details");
-//        }
-//    }
-//}
+        public IActionResult Details(int id)
+        {
+            return View(_db.Teams.FirstOrDefault(team => team.Id == id));
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> Details(string name)
+        //{
+        //    Team newTeam = new Team(name);
+        //    newTeam.Users.Add(await _userManager.FindByIdAsync(User.Claims.));
+        //    _db.Teams.Add(newTeam);
+        //    _db.SaveChanges();
+        //    return RedirectToAction("Details");
+        //}
+    }
+}
--- src/ArenaRoster/Models/Team.cs
+++ src/ArenaRoster/Models/Team.cs
@@ -1,17 +1,18 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using System.ComponentModel.DataAnnotations;
-//using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
-//namespace ArenaRoster.Models
-//{
-//    public class Team
-//    {
-//        [Key]
-//        public int TeamId { get; set; }
-//        public string Name { get; set; }
-//        public List<PlayerTeam> PlayerTeams { get; set; } 
-//    }
-//}
+namespace ArenaRoster.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual List<PlayerTeam> Roster { get; set; }
+    }
+}
