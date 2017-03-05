909e80d9feac45fc5370e352b6799e4b5a2c537b
Levi Bibo <levibibo@gmail.com>
2017-03-05 11:08:16 -0800

    Add Mailgun Api call for sending automated email on new player signup.

---
+++ src/ArenaRoster/Models/MailgunApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using System.Diagnostics;
+using RestSharp.Authenticators;
+
+namespace ArenaRoster.Models
+{
+    public class MailgunApi
+    {
+        public static void SendMailgunMessage(string recipient, string teamName, string password)
+        {
+            RestClient client = new RestClient("https://api.mailgun.net/v3");
+            client.Authenticator =
+                new HttpBasicAuthenticator("api", $"{EnvironmentVariables.MailgunKey}");
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", $"{EnvironmentVariables.MailgunDomain}", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", $"ArenaRoster <mailgun@{EnvironmentVariables.MailgunDomain}>");
+            request.AddParameter("to", $"{recipient}");
+            request.AddParameter("subject", $"Invitation to join team {teamName}");
+            request.AddParameter("text", $"You have been invited to join the team {teamName} on ArenaRoster.  Please sign in using the following credentials:\nEmail: {recipient}\nPassword: {password}");
+            request.Method = Method.POST;
+
+            RestResponse response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            TaskCompletionSource<IRestResponse> tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
