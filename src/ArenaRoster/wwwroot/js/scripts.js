dffbbd94912a3190a979951c5d6f9ef46d9a2ff5
Levi Bibo <levibibo@gmail.com>
2017-03-07 14:44:39 -0800

    Add mobile nav bar.

--- src/ArenaRoster/wwwroot/js/scripts.js
+++ src/ArenaRoster/wwwroot/js/scripts.js
@@ -1,8 +1,14 @@
 ﻿$(document).ready(function () {
+    $(".button-collapse").sideNav({
+        menuWidth: '100%',
+        closeOnClick: true,
+        draggable: true
+    });
     $('select').material_select();
     $('.collapsible').collapsible();
     $('.datepicker').pickadate({
         selectMonths: true,
         selectYears: 15
     });
+
 });
