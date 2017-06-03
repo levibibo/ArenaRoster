ce4de46c51800350a426fdb98add9a6696dce38a
levibibo <levibibo@gmail.com>
2017-06-02 23:56:57 -0700

    Convert team display on user account page to Vue instance.

---
+++ src/ArenaRoster/wwwroot/js/Account/index.js
@@ -0,0 +1,25 @@
+﻿new Vue({
+    el: "#user-teams",
+
+    data: {
+        
+    },
+
+    methods: {
+
+    },
+
+    created: function () {
+        $.ajax({
+            type: 'GET',
+            url: 'Account/GetTeams',
+            success: function (result) {
+                $("#user-teams").html(result);
+            },
+            error: function (result) {
+                $("#user-teams").html("We ran into a problem getting your teams.  Please try again later.")
+                console.log(result);
+            }
+        })
+    }
+})
