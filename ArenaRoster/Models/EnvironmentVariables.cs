eec77e02459f535fa8dadc0baf40c2d7e1904e55
levibibo <levibibo@gmail.com>
2017-08-01 20:46:41 -0700

    Change directory structure.

---
+++ ArenaRoster/Models/EnvironmentVariables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecTeam.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string MailgunKey = "";
+        public static string MailgunDomain = "";
+        public static string DbUserId = "levibibo@recteamdbserver";
+        public static string DbPassword = "1162Cool";
+    }
+}
